e524fec889e5f28800420a76610fed51af9f6b11
alohalilpeep <alohalilpeep@gmail.com>
2025-06-10 21:20:23 +0300

    add n test ci cd 3

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,3 +25,27 @@ tasks.test {
     useJUnitPlatform()
 }
 
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])  // Публикует JAR-файл
+            // Дополнительные метаданные (опционально)
+            pom {
+                name.set("Bicycle Rental")
+                description.set("Library for bicycle rental management")
+                url.set("https://github.com/alohalilpeep/bicycle_rental")
+            }
+        }
+    }
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/alohalilpeep/bicycle_rental")
+            credentials {
+                username = System.getenv("GITHUB_ACTOR")
+                password = System.getenv("GITHUB_TOKEN")
+            }
+        }
+    }
+}
+
